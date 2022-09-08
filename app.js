ae21f50e5bd2907a6c07281713e3abe218601602
micahco <micahcowell@gmail.com>
2022-09-08 12:07:19 -0700

    working

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-import * as cheerio from 'cheerio';
+const puppeteer = require('puppeteer');
+
+(async () => {
+	const browser = await puppeteer.launch()
+	const page = await browser.newPage()
+
+	const URL = 'https://rateyourmusic.com/release/album/friendzone/dx-vol-2/'
+	console.log('GOTO: ' + URL)
+	await page.goto(URL)
+
+	let data = []
+	while (true) {
+		let newResults = await page.evaluate(() => {
+			let results = []
+			let catalog = document.querySelectorAll('.catalog_line')
+			catalog.forEach((catalog_line) => {
+				results.push({
+					user: catalog_line.querySelector('div.catalog_header > span.catalog_user > a').textContent,
+					rating: catalog_line.querySelector('div.catalog_header > span.catalog_rating > img').getAttribute('title').replace(' stars', ''),
+				})
+			})
+			return results
+		})
+		data = data.concat(newResults)
+		if (await page.$('a.navlinknext') == null) {
+			break
+		}
+		let navlinkcurrent = await page.$eval('.navlinkcurrent', el => el.textContent)
+		await page.$eval('a.navlinknext', elem => (elem).click());
+		await page.waitForFunction(`document.querySelector('.navlinkcurrent').textContent != ${navlinkcurrent}`);
+	}
+	console.log(data)
+
+	console.log('closing browser')
+	await browser.close()
+})();
