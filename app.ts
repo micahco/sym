3eaef7584a413137e830980088911fd46f7f690e
micahco <micahcowell@gmail.com>
2022-09-08 19:51:42 -0700

    classy

--- app.ts
+++ app.ts
@@ -1,106 +1,123 @@
 import { launch } from 'puppeteer';
 
-interface UserRating {
-	user: string | null;
+interface CatalogLine {
+	username: string | null;
 	stars: number;
 }
 
-interface ReleaseData {
-	URL: string | null;
+interface Release {
+	url: string | null;
 	title: string | null;
 	artist: string | null;
-	userRatings: UserRating[];
+}
+
+interface CatalogData {
+	release: Release;
+	catalogLines: CatalogLine[];
 }
 
 interface Rating {
-	URL: string | null;
-	title: string | null;
-	artist: string | null;
+	release: Release;
 	stars: number;
 }
 
-// max number of rating page iterations (15 ratings per page)
-const PAGE_INDEX_LIMITER: number = 10;
+class App {
+	private catalog: CatalogData[];
+	private pageIndexLimiter: number;
+	private urls: string[];
 
-(async () => {
-	const URLs: string[] = [
-		'https://rateyourmusic.com/release/unauth/hank-williams-iii/boot-2/',
-		'https://rateyourmusic.com/release/unauth/hank-williams-iii/boot-3/'
-	];
-
-	let userKey: any[] = [];
-	let ratings: Rating[][] = []
-	for (const URL of URLs) {
-		const release: ReleaseData = await getRelease(URL);
-		for (const ur of release.userRatings) {
-			const r: Rating = {
-				URL: release.URL,
-				title: release.title,
-				artist: release.artist,
-				stars: ur.stars
-			}
-			const indexOfUser = userKey.indexOf(ur.user);
-			if (indexOfUser === -1) {
-				userKey.push(ur.user)
-				ratings.push([r])
-			} else {
-				ratings[indexOfUser].push(r)
-			}
-		}
+	constructor(pageIndexLimiter: number) {
+		this.catalog = [];
+		this.pageIndexLimiter = pageIndexLimiter;
+		this.urls = [
+			'https://rateyourmusic.com/release/unauth/hank-williams-iii/boot-2/',
+			'https://rateyourmusic.com/release/unauth/hank-williams-iii/boot-3/'
+		];
 	}
 
-})();
+	public async run(): Promise<void> {
+		for (const url of this.urls) {
+			await this.scrape(url);
+		}
+		this.parse()
+	}
 
-async function getRelease(URL: string): Promise<ReleaseData> {
+	private async scrape(url: string): Promise<void> {
 
-	// launch puppeteer
-	const browser = await launch();
-	const page = await browser.newPage();
-	console.log(`scraping: ${URL}`);
-	await page.goto(URL);
-
-	// scrape pages for user ratings
-	let userRatings: UserRating[] = [];
-	let pageIndex = 1;
-	while (pageIndex < PAGE_INDEX_LIMITER) {
-
-		// get this page user ratings
-		let pageUserRatings: UserRating[] = await page.evaluate(() => {
-			let data: UserRating[] = [];
-			document.querySelectorAll('.catalog_line').forEach((el: Element) => {
-				const ur: UserRating = {
-					user: el.querySelector('div.catalog_header > span.catalog_user > a')!.textContent,
-					stars: parseFloat(el.querySelector('div.catalog_header > span.catalog_rating > img')!.getAttribute('title')!.replace(' stars', ''))
-				}
-				data.push(ur);
+		// launch puppeteer
+		const browser = await launch();
+		const page = await browser.newPage();
+		console.log(`scraping: ${url}`);
+		await page.goto(url);
+	
+		// scrape pages for data
+		let catalogLines: CatalogLine[] = [];
+		let pageIndex = 1;
+		while (pageIndex < this.pageIndexLimiter) {
+	
+			// page data
+			let pageCatalogLines: CatalogLine[] = await page.evaluate(() => {
+				let data: CatalogLine[] = [];
+				document.querySelectorAll('.catalog_line').forEach((el: Element) => {
+					const cl: CatalogLine = {
+						username: el.querySelector('div.catalog_header > span.catalog_user > a')!.textContent,
+						stars: parseFloat(el.querySelector('div.catalog_header > span.catalog_rating > img')!.getAttribute('title')!.replace(' stars', ''))
+					}
+					data.push(cl);
+				})
+				return data;
 			})
-			return data;
-		})
-		userRatings = userRatings.concat(pageUserRatings);
-
-		// check if next page exists
-		if (await page.$('a.navlinknext') == null) {
-			break;
+			catalogLines = catalogLines.concat(pageCatalogLines);
+	
+			// check if next page exists
+			if (await page.$('a.navlinknext') == null) {
+				break;
+			}
+	
+			// get current page number
+			let navlinkcurrent = await page.$eval('.navlinkcurrent', (el: any) => el.textContent);
+	
+			// click next page button
+			await page.$eval('a.navlinknext', (el: any) => (el).click());
+	
+			// wait for page to change
+			await page.waitForFunction(`document.querySelector('.navlinkcurrent').textContent != ${navlinkcurrent}`);
+	
+			pageIndex++;
 		}
-
-		// get current page number
-		let navlinkcurrent = await page.$eval('.navlinkcurrent', (el: any) => el.textContent);
-
-		// click next page button
-		await page.$eval('a.navlinknext', (el: any) => (el).click());
-
-		// wait for page to change
-		await page.waitForFunction(`document.querySelector('.navlinkcurrent').textContent != ${navlinkcurrent}`);
-
-		pageIndex++;
+	
+		// scrape release information from page
+		const title = await page.$eval('div.album_title', (el: any) => el.innerText);
+		const artist = await page.$eval('a.artist', (el: any) => el.innerText);
+		const release = { url, title, artist }
+	
+		// close puppeteer
+		await browser.close();
+	
+		this.catalog.push({ release, catalogLines });
 	}
 
-	// scrape release information from page
-	const title = await page.$eval('div.album_title', (el: any) => el.innerText);
-	const artist = await page.$eval('a.artist', (el: any) => el.innerText);
-
-	// close puppeteer
-	await browser.close();
-
-	return { URL, title, artist, userRatings };
+	private parse(): void {
+		let userKey: any[] = [];
+		let ratings: Rating[][] = []
+		for (const cd of this.catalog) {
+			for (const cl of cd.catalogLines) {
+				const r: Rating = {
+					release: cd.release,
+					stars: cl.stars
+				}
+				const indexOfUser = userKey.indexOf(cl.username);
+				if (indexOfUser === -1) {
+					userKey.push(cl.username)
+					ratings.push([r])
+				} else {
+					ratings[indexOfUser].push(r)
+				}
+			}
+		}
+		console.log(userKey)
+	}
 }
+
+let app = new App(10);
+app.run();
