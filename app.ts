4eb7b29e0bb65d32075868d6987956969aa9c3ce
micahco <micahcowell@gmail.com>
2022-09-10 15:45:46 -0700

    restructure

--- app.ts
+++ app.ts
@@ -1,8 +1,7 @@
 import * as fs from 'fs';
 import * as readline from 'readline';
 import { stdout } from 'process';
-import { launch } from 'puppeteer';
-// adblock
+import { launch, Page } from 'puppeteer';
 import { PuppeteerBlocker } from '@cliqz/adblocker-puppeteer';
 import fetch from 'cross-fetch';
 
@@ -17,18 +16,14 @@ interface CatalogLine {
 }
 
 interface CatalogData {
-	release: Release;
+	title: string;
+	artist: string;
 	catalogLines: CatalogLine[];
 }
 
-interface Release {
-	url: string;
+interface Rating {
 	title: string;
 	artist: string;
-}
-
-interface Rating {
-	release: Release;
 	stars: number;
 }
 
@@ -36,44 +31,18 @@ declare const RYMmediaPage: any;
 
 class App {
 	private cfg: Config;
-	private urls: string[];
-	private catalog: CatalogData[];
-	private userData: Map<string, Rating[]>;
-	private matchData: Map<string, Rating[]> | null;
 
 	constructor(cfg: Config) {
 		this.cfg = cfg;
-		this.urls = [];
-		this.catalog = [];
-		this.userData = new Map();
-		this.matchData = null;
 	}
 
 	public async run(): Promise<void> {
-		await this.read();
-		for (const url of this.urls) {
-			await this.scrape(url);
-		}
-		this.parse();
-		this.analyze();
-		console.log(this.matchData);
-	}
-
-	private async read() {
-		const fileStream = fs.createReadStream('urls.txt');
-		const rl = readline.createInterface({
-			input: fileStream,
-			crlfDelay: Infinity
-		});
-		for await (const line of rl) {
-			this.urls.push(line)
-		}
-	}
-
-	private async scrape(url: string): Promise<void> {
 
 		// launch puppeteer
-		const browser = await launch( {headless: true} );
+		const browser = await launch({
+			headless: true,
+			timeout: 0
+		});
 		const page = await browser.newPage();
 
 		// adblock
@@ -81,21 +50,56 @@ class App {
 			blocker.enableBlockingInPage(page);
 		});
 
-		// open url
-		console.log(`${url}`);	
+		// get urls from txt
+		const urls = await this.getLinesFromFile('urls.txt');
+		
+		// scrape catalog data from each url
+		const catalog = []
+		for (const url of urls) {
+			const cd = await this.getCatalogData(page, url);
+			catalog.push(cd);
+		}
+
+		// close puppeteer
+		await browser.close();
+
+		// generate data
+		const data = this.parseCatalog(catalog);
+		const arr = this.filterDataToArray(data);
+		const json = JSON.stringify(arr, null, '\t');
+		fs.writeFileSync('data.json', json);
+		console.log('saved: data.json')
+	}
+
+	private async getLinesFromFile(filename: string): Promise<string[]> {
+		const fileStream = fs.createReadStream(filename);
+		const rl = readline.createInterface({
+			input: fileStream,
+			crlfDelay: Infinity
+		});
+		const lines: string[] = [];
+		for await (const line of rl) {
+			lines.push(line)
+		}
+		return lines;
+	}
+
+	private async getCatalogData(page: Page, url: string): Promise<CatalogData> {
+
+		// open url;	
 		await page.goto(url, { waitUntil: "domcontentloaded" });
 
-		// scrape release information from page
-		const id = await page.$eval('input.album_shortcut', (el: any) => el.value.replace(/[^0-9]/g, ''))
+		// scrape release information
+		const id = await page.$eval('input.album_shortcut', (el: any) => el.value.replace(/[^0-9]/g, ''));
 		const title = await page.$eval('div.album_title', (el: any) => el.innerText);
 		const artist = await page.$eval('a.artist', (el: any) => el.innerText);
-		const release = { url, title, artist }
+		console.log(`scraping: ${artist} - ${title}`);
 		
 		// scrape pages for data
 		let catalogLines: CatalogLine[] = [];
 		let pageIndex = 1;
 		while (pageIndex <= this.cfg.pageLimit) {
-			stdout.write(`Scraping page ${pageIndex}\r`);
+			stdout.write(`page ${pageIndex}\r`);
 
 			// page data
 			let pageCatalogLines: CatalogLine[] = await page.evaluate(() => {
@@ -126,48 +130,56 @@ class App {
 			const sel = '#catalog_list > span > span.navlinkcurrent';
 			await page.waitForFunction((cur, sel) => {
 				return document.querySelector(sel)?.textContent != cur
-			}, 
-				{polling: 'mutation'}, pageIndex.toString(), sel);
+			}, {
+				timeout: 0,
+				polling: 'mutation'
+			}, pageIndex.toString(), sel);
 
 			pageIndex++;
 		}
-		console.log('\ndone');
-	
-		// close puppeteer
-		await browser.close();
-		this.catalog.push({ release, catalogLines });
+		stdout.write(`\t\u2705\r`); //	✔
+		return { title, artist, catalogLines };
 	}
 
-	private parse(): void {
-		for (const cd of this.catalog) {
+	private parseCatalog(catalog: CatalogData[]): Map<string, Rating[]> {
+		const data: Map<string, Rating[]> = new Map();
+		for (const cd of catalog) {
 			for (const cl of cd.catalogLines) {
-				const r: Rating = {
-					release: cd.release,
+				const newRating: Rating = {
+					title: cd.title,
+					artist: cd.artist,
 					stars: cl.stars
 				}
 				let user = cl.username;
-				let ratings = [r];
-				if (this.userData.has(user)) {
-					ratings = ratings.concat(this.userData.get(user) as Rating[]);
+				let newRatingList = [newRating];
+				if (data.has(user)) {
+					const oldRatingList = data.get(user) as Rating[];
+					// check if rating is duplicate
+					for (const oldRating of oldRatingList) {
+						if (oldRating.title == newRating.title) {
+							newRatingList = [];
+							break;
+						}
+					}
+					newRatingList = newRatingList.concat(oldRatingList);
 				}
-				this.userData.set(user, ratings)
+				data.set(user, newRatingList)
 			}
 		}
+		return data;
 	}
 
-	private analyze(): void {
-		this.matchData = new Map(
-			Array.from(this.userData).filter(([user, ratings]) => {
-				return ratings.length > this.cfg.matchCount;
-			})
-		);
+	private filterDataToArray(data: Map<string, Rating[]>): [string, Rating[]][] {
+		return Array.from(data).filter(([user, ratings]) => {
+			return ratings.length > this.cfg.matchCount;
+		})
 	}
 }
 
 (async () => {
 	console.time('total')
 	const app = new App({
-		pageLimit: 10,
+		pageLimit: 20,
 		matchCount: 1
 	});
 	await app.run();
