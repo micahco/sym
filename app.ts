4b61147a89bb05b4eca238c8a1375b875d70f752
micahco <micahcowell@gmail.com>
2022-09-10 10:57:15 -0700

    v0.0.2

--- app.ts
+++ app.ts
@@ -1,16 +1,19 @@
 import * as fs from 'fs';
 import * as readline from 'readline';
+import { stdout } from 'process';
 import { launch } from 'puppeteer';
+// adblock
+import { PuppeteerBlocker } from '@cliqz/adblocker-puppeteer';
+import fetch from 'cross-fetch';
 
-interface CatalogLine {
-	username: string | null;
-	stars: number;
+interface Config {
+	pageLimit: number;
+	matchCount: number;
 }
 
-interface Release {
-	url: string | null;
-	title: string | null;
-	artist: string | null;
+interface CatalogLine {
+	username: string;
+	stars: number;
 }
 
 interface CatalogData {
@@ -18,20 +21,32 @@ interface CatalogData {
 	catalogLines: CatalogLine[];
 }
 
+interface Release {
+	url: string;
+	title: string;
+	artist: string;
+}
+
 interface Rating {
 	release: Release;
 	stars: number;
 }
 
-class App {
-	private pageIndexLimiter: number;
-	private catalog: CatalogData[];
-	private urls: string[];
+declare const RYMmediaPage: any;
 
-	constructor(pageIndexLimiter: number) {
-		this.pageIndexLimiter = pageIndexLimiter;
-		this.catalog = [];
+class App {
+	private cfg: Config;
+	private urls: string[];
+	private catalog: CatalogData[];
+	private userData: Map<string, Rating[]>;
+	private matchData: Map<string, Rating[]> | null;
+
+	constructor(cfg: Config) {
+		this.cfg = cfg;
 		this.urls = [];
+		this.catalog = [];
+		this.userData = new Map();
+		this.matchData = null;
 	}
 
 	public async run(): Promise<void> {
@@ -39,7 +54,9 @@ class App {
 		for (const url of this.urls) {
 			await this.scrape(url);
 		}
-		this.parse()
+		this.parse();
+		this.analyze();
+		console.log(this.matchData);
 	}
 
 	private async read() {
@@ -56,22 +73,36 @@ class App {
 	private async scrape(url: string): Promise<void> {
 
 		// launch puppeteer
-		const browser = await launch();
+		const browser = await launch( {headless: true} );
 		const page = await browser.newPage();
-		console.log(`scraping: ${url}`);
-		await page.goto(url);
-	
+
+		// adblock
+		PuppeteerBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
+			blocker.enableBlockingInPage(page);
+		});
+
+		// open url
+		console.log(`${url}`);	
+		await page.goto(url, { waitUntil: "domcontentloaded" });
+
+		// scrape release information from page
+		const id = await page.$eval('input.album_shortcut', (el: any) => el.value.replace(/[^0-9]/g, ''))
+		const title = await page.$eval('div.album_title', (el: any) => el.innerText);
+		const artist = await page.$eval('a.artist', (el: any) => el.innerText);
+		const release = { url, title, artist }
+		
 		// scrape pages for data
 		let catalogLines: CatalogLine[] = [];
 		let pageIndex = 1;
-		while (pageIndex < this.pageIndexLimiter) {
-	
+		while (pageIndex <= this.cfg.pageLimit) {
+			stdout.write(`Scraping page ${pageIndex}\r`);
+
 			// page data
 			let pageCatalogLines: CatalogLine[] = await page.evaluate(() => {
 				let data: CatalogLine[] = [];
 				document.querySelectorAll('.catalog_line').forEach((el: Element) => {
 					const cl: CatalogLine = {
-						username: el.querySelector('div.catalog_header > span.catalog_user > a')!.textContent,
+						username: el.querySelector('div.catalog_header > span.catalog_user > a')!.textContent as string,
 						stars: parseFloat(el.querySelector('div.catalog_header > span.catalog_rating > img')!.getAttribute('title')!.replace(' stars', ''))
 					}
 					data.push(cl);
@@ -83,24 +114,24 @@ class App {
 			// check if next page exists
 			if (await page.$('a.navlinknext') == null) {
 				break;
-			}
-	
-			// get current page number
-			let navlinkcurrent = await page.$eval('.navlinkcurrent', (el: any) => el.textContent);
-	
-			// click next page button
-			await page.$eval('a.navlinknext', (el: any) => (el).click());
+			}		
+
+			// go to next page
+			const nextPage = '/' + (pageIndex + 1);
+			await page.evaluate((id, nextPage)=> {
+				RYMmediaPage.navCatalog('l', id, true, 'ratings', nextPage);
+			}, id, nextPage)
 	
 			// wait for page to change
-			await page.waitForFunction(`document.querySelector('.navlinkcurrent').textContent != ${navlinkcurrent}`);
-	
+			const sel = '#catalog_list > span > span.navlinkcurrent';
+			await page.waitForFunction((cur, sel) => {
+				return document.querySelector(sel)?.textContent != cur
+			}, 
+				{polling: 'mutation'}, pageIndex.toString(), sel);
+
 			pageIndex++;
 		}
-	
-		// scrape release information from page
-		const title = await page.$eval('div.album_title', (el: any) => el.innerText);
-		const artist = await page.$eval('a.artist', (el: any) => el.innerText);
-		const release = { url, title, artist }
+		console.log('\ndone');
 	
 		// close puppeteer
 		await browser.close();
@@ -108,30 +139,37 @@ class App {
 	}
 
 	private parse(): void {
-		let userKey: any[] = [];
-		let ratings: Rating[][] = []
 		for (const cd of this.catalog) {
 			for (const cl of cd.catalogLines) {
 				const r: Rating = {
 					release: cd.release,
 					stars: cl.stars
 				}
-				const indexOfUser = userKey.indexOf(cl.username);
-				if (indexOfUser === -1) {
-					userKey.push(cl.username)
-					ratings.push([r])
-				} else {
-					ratings[indexOfUser].push(r)
+				let user = cl.username;
+				let ratings = [r];
+				if (this.userData.has(user)) {
+					ratings = ratings.concat(this.userData.get(user) as Rating[]);
 				}
+				this.userData.set(user, ratings)
 			}
 		}
-		console.log(userKey)
+	}
+
+	private analyze(): void {
+		this.matchData = new Map(
+			Array.from(this.userData).filter(([user, ratings]) => {
+				return ratings.length > this.cfg.matchCount;
+			})
+		);
 	}
 }
 
 (async () => {
-	console.time('run')
-	let app = new App(10);
+	console.time('total')
+	const app = new App({
+		pageLimit: 10,
+		matchCount: 1
+	});
 	await app.run();
-	console.timeEnd('run')
+	console.timeEnd('total');
 })();
